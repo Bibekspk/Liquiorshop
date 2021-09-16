e0d52681349036b6c9828324c276f495f818aa6a
Bibekspk <bibeksp7@gmail.com>
2021-09-16 16:00:22 +0545

    changes

--- src/components/page/contact.js
+++ src/components/page/contact.js
@@ -17,8 +17,11 @@ export class Contact extends Component {
                     <div className="col-md-5 text-center">
                         <p>We are open for delivery.</p>
                         <p>Contact us for home delivery.</p>
-                        <i style={{ fontSize: "25px", backgroundColor: "white" }} class="far fa-phone-alt"></i><span> Call Us</span>
-                        <span><p>+977 986-3450-879</p></span>
+                        <i style={{ fontSize: "25px", backgroundColor: "white" }} class="far fa-phone-alt"></i><span> Call Us </span>
+                        <span><p>+977 986-0673-130</p></span>
+                        <span><p>+977 986-0673-119</p></span>
+                        <i style={{ fontSize: "25px", backgroundColor: "white" }} class="far fa-map-marker-alt"></i><span> Delivery Area</span>
+                        <span><p>Chitwan and Gaindakot</p></span>
                     </div>
                     <form className="form col-md-6 mt-3 mx-2">
                         <input type="text" placeholder="Name" required></input><br /><br />
