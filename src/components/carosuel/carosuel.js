941de36638f701a56e70eb464f1a0b40d1177edd
Bibekspk <bibeksp7@gmail.com>
2021-09-19 22:23:47 +0545

    changes

--- src/components/carosuel/carosuel.js
+++ src/components/carosuel/carosuel.js
@@ -21,7 +21,7 @@ export const CarosuelSilder = () => {
                     </div>
                 </div>
                 <div>
-                    <img src={img1} height="550px" />
+                    <img src="https://cdn.luxe.digital/media/2020/07/24153549/johnnie-walker-luxury-whiskey-luxe-digital%402x.jpg" height="550px" />
                     <div className="text row">
                         <div className="col-md-1"></div>
                         <div className="col-md-4">
@@ -31,11 +31,11 @@ export const CarosuelSilder = () => {
                     </div>
                 </div>
                 <div>
-                    <img src={img2} height="550px" />
+                    <img src="https://media.glenfiddich.com/assets/Uploads/Shared/Products/Malt-Master-s-Edition/glenfiddich-malt-master-s-edition-main.jpg" height="550px" />
                     <div className="text row">
                         <div className="col-md-1"></div>
                         <div className="col-md-4">
-                            <h1 className="mt-5">POP UP HAPPINESS</h1>
+                            <h1 className="mt-5" >POP UP HAPPINESS</h1>
                             <button  style={{ fontSize: "22px",width:"auto" }} className="btn btn-primary"><a href="#CONTACT">CONTACT US</a> </button>
 
                         </div>
--- src/components/navbar/topnavbar.js
+++ src/components/navbar/topnavbar.js
@@ -9,7 +9,7 @@ export const TopNavBar=()=> {
                 <ul className="navlist">
                     <li className="navitem"><a target="_blank" rel="noreferrer" href="https://www.facebook.com/boozechitwan"><i class="fab fa-facebook"></i></a></li>
                     <li className="navitem"><a href="/"><i class="fab fa-instagram "></i></a></li>
-                    <li className="navitem">Delivery Hours : <strong><i class="fas fa-history"></i>  24 Hours Instant Delivery</strong> </li>
+                    <li className="navitem" style={{color:"red", fontSize:"22px"}}>Delivery Hours : <strong><i style={{color:"red"}} class="fas fa-history"></i>  24 Hours Instant Delivery</strong> </li>
                     <li className="navitem num"><i style={{ fontSize: "20px", backgroundColor: "white" }} class="far fa-phone-alt"></i><p >&nbsp;986-0673-119, 986-0673-130</p> </li>
                 </ul>
             </div>
